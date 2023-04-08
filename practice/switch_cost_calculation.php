6093fe125794cad58d99e6b27afed1ba497ab5d4
Wijayaac <wijayaac25@gmail.com>
2023-04-09 07:00:24 +0800

    update : switch case cost calculation process

--- practice/switch_cost_calculation.php
+++ practice/switch_cost_calculation.php
@@ -1,47 +1,41 @@
 <?php
 $unitLength = isset($_GET['unit']) ? strtolower($_GET['unit']) : "m";
-$anggrekStreetLength = isset($_GET['anggrek_street']) ? $_GET['anggrek_street'] : 0;
-$kambojaStreetLength = isset($_GET['kamboja_street']) ? $_GET['kamboja_street'] : 0;
-$lotusStreetLength = isset($_GET['lotus_street']) ? $_GET['lotus_street'] : 0;
+$anggrekStreetLength = $_GET['anggrek_street'] ?? 0;
+$kambojaStreetLength = $_GET['kamboja_street'] ?? 0;
+$lotusStreetLength = $_GET['lotus_street'] ?? 0;
 
 $isCashReady = isset($_GET['is_cash_available']) ? true : false;
-$dividerValue = 1;
+
+$materialCost = 15000;
+$serviceCost = 650000 / 1000;
 
 switch ($unitLength) {
     case 'km':
         $anggrekStreetLength *= 1000;
         $kambojaStreetLength *= 1000;
         $lotusStreetLength *= 1000;
-        $serviceCost = 650000 / 1;
         break;
     case 'cm':
-        $anggrekStreetLength /= 1000;
-        $kambojaStreetLength /= 1000;
-        $lotusStreetLength /= 1000;
-        $serviceCost = 650000 / 1000000;
+        $anggrekStreetLength /= 100;
+        $kambojaStreetLength /= 100;
+        $lotusStreetLength /= 100;
         break;
 
     default:
-        $anggrekStreetLength = $anggrekStreetLength * 1;
-        $kambojaStreetLength = $kambojaStreetLength * 1;
-        $lotusStreetLength = $lotusStreetLength * 1;
-        $serviceCost = 650000 / 1000;
         break;
 }
 
 // count the total street
 $totalStreetLength = $anggrekStreetLength + $kambojaStreetLength + $lotusStreetLength;
 
-$materialCost = 15000;
-
-// $serviceCost = 650000 / 1000; // km to m
-
 // Total service and material cost
 $totalAdditionalCost = $materialCost + $serviceCost;
+var_dump([$anggrekStreetLength, $kambojaStreetLength, $lotusStreetLength]);
 
 // Total cost 
 $totalCost = $totalStreetLength * $totalAdditionalCost;
 
+// Prevent negative total in cost calculation
 if ($totalStreetLength < 0) {
     $totalCost = 0;
     $totalStreetLength = 0;
@@ -85,20 +79,20 @@ switch ($isCashReady) {
             <label for="unit">Unit Length:</label>
             <select name="unit" id="unit">
                 <option value="km">KM</option>
-                <option value="km">M</option>
-                <option value="km">CM</option>
+                <option value="m">M</option>
+                <option value="cm">CM</option>
             </select>
         </div>
         <div>
-            <label for="anggrek_street">Anggrek Street (KM):</label>
+            <label for="anggrek_street">Anggrek Street:</label>
             <input type="number" step="any" name="anggrek_street" id="anggrek_street" required>
         </div>
         <div>
-            <label for="kamboja_street">Kamboja Street (M):</label>
+            <label for="kamboja_street">Kamboja Street:</label>
             <input type="number" step="any" name="kamboja_street" id="kamboja_street" required>
         </div>
         <div>
-            <label for="lotus_street">Lotus Street (CM):</label>
+            <label for="lotus_street">Lotus Street:</label>
             <input type="number" step="any" name="lotus_street" id="lotus_street" required>
         </div>
         <div>
@@ -108,7 +102,7 @@ switch ($isCashReady) {
         <button type="submit">Submit</button>
     </form>
 
-    <?php if ($totalStreetLength > 0) : ?>
+    <?php if ($totalStreetLength) : ?>
         <p>Description : To carry out road repairs with a total length of <?= number_format($totalStreetLength, 0) ?> meters, Perumahan Graha Sentosa must prepare a total cost of Rp. <?= number_format($totalCost, 2) ?></p>
     <?php endif ?>
 
